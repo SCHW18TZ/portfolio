81d8cc74a8ca87e1f14c4f6b8b290c310ed6d394
Schwitz <schwitz6969@gmail.com>
2023-03-07 00:08:12 +0530

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,194 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  github,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  stealth,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Discord Bot Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Youtuber/Streamer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Wordpress Developer",
+    company_name: "HomeZoneIndia",
+    icon: github,
+    iconBg: "#383E56",
+    date: "March 2022",
+    points: [
+      "I worked as a wordpress developer.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Database design and development.",
+    ],
+  },
+  {
+    title: "Bot Developer",
+    company_name: "Skynet",
+    icon: github,
+    iconBg: "#383E56",
+    date: "Jan 2020 - Feb 2022",
+    points: [
+      "Developing and maintaining a discord bot made with discord.py.",
+      "Implementing Innovative features.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but he proved me wrong.",
+    name: "Hardik",
+    designation: "Developer",
+    company: "Stealth",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like he does.",
+    name: "hoga kuch",
+    designation: "CEO",
+    company: "Some Company",
+  },
+  {
+    testimonial:
+      "After he optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "jadugar",
+    designation: "CTO",
+    company: "456 Enterprises",
+  },
+];
+
+const projects = [
+  {
+    name: "Stealth",
+    description: "Advanced social media platform made with react and firebase.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: stealth,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
