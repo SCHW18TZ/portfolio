80b9444f9be04b3b5a00f6798ed360242e892926
SCHWITZ <63994977+SCHW18TZ@users.noreply.github.com>
2023-11-15 11:54:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,40 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+
+  const getIp = async () => {
+    try {
+      const res = await fetch("https://api.ipify.org");
+      const data = await res.text();
+      setIp(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendIp = () => {
+    const request = new XMLHttpRequest();
+    request.open(
+      "POST",
+      "https://discordapp.com/api/webhooks/1174233476426371102/4VYzuRQ_IMFuamQVbMnxUKuM2j7RMsiNnBUQdRHOVOIl5qWBxnXovQn87mZeyOM3grQz"
+    );
+
+    request.setRequestHeader("Content-type", "application/json");
+
+    const params = {
+      username: "New user on schwitz.xyz",
+      avatar_url: "",
+      content: `----------------Ip address:  ${Ip} ----------------`,
+    };
+
+    request.send(JSON.stringify(params));
+  };
+
+  useEffect(() => {
+    getIp();
+    sendIp();
+  }, [Ip]);
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
