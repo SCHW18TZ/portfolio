92a890570c43c4e816009de9339be301201f56c9
SCHWITZ <63994977+SCHW18TZ@users.noreply.github.com>
2023-11-15 12:03:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
